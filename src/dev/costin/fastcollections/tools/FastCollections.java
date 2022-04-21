fc393d18399361104546941096f1cdfb6b149edb
Costin <Costin@BUUMBUUM>
2022-04-21 16:02:11 +0200

    Add unmodifiable collection building functions.

--- src/dev/costin/fastcollections/tools/FastCollections.java
+++ src/dev/costin/fastcollections/tools/FastCollections.java
@@ -3,6 +3,7 @@ package dev.costin.fastcollections.tools;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
+import java.util.function.Consumer;
 
 import dev.costin.fastcollections.IntCollection;
 import dev.costin.fastcollections.IntComparator;
@@ -11,6 +12,7 @@ import dev.costin.fastcollections.IntIterator;
 import dev.costin.fastcollections.lists.IntList;
 import dev.costin.fastcollections.maps.IntDoubleMap;
 import dev.costin.fastcollections.maps.IntIntMap;
+import dev.costin.fastcollections.maps.IntLongMap;
 import dev.costin.fastcollections.maps.IntObjectMap;
 import dev.costin.fastcollections.sets.IntSet;
 import dev.costin.fastcollections.sets.impl.IntGrowingSet;
@@ -23,13 +25,13 @@ import dev.costin.fastcollections.sets.impl.IntGrowingSet;
  *
  */
 public class FastCollections {
-   
+
    public static final int DEFAULT_LIST_CAPACITY = 8;
 
-   public static IntGrowingSet newIntRangeSetWithElements( int...elements ) {
+   public static IntGrowingSet newIntRangeSetWithElements( int... elements ) {
       int min = elements[0];
       int max = min;
-      for( int i=1; i<elements.length; i++ ) {
+      for( int i = 1; i < elements.length; i++ ) {
          if( elements[i] < min ) {
             min = elements[i];
          }
@@ -37,13 +39,13 @@ public class FastCollections {
             max = elements[i];
          }
       }
-      
-      final IntGrowingSet set = new IntGrowingSet(min, max, elements.length+1);
+
+      final IntGrowingSet set = new IntGrowingSet( min, max, elements.length + 1 );
       set.addAll( elements );
-      
+
       return set;
    }
-   
+
    public static IntCollection emptyIntCollection() {
       return EMPTY_INT_COLLECTION;
    }
@@ -51,29 +53,75 @@ public class FastCollections {
    public static IntList emptyIntList() {
       return EMPTY_INT_LIST;
    }
-   
+
    public static IntSet emptyIntSet() {
       return EMPTY_INT_SET;
    }
-   
+
    public static IntIntMap emptyIntIntMap() {
       return EMPTY_INT_INT_MAP;
    }
-   
+
    public static IntDoubleMap emptyIntDoubleMap() {
       return EMPTY_INT_DOUBLE_MAP;
    }
-   
+
    @SuppressWarnings( "unchecked" )
    public static <T> IntObjectMap<T> emptyIntObjectMap() {
       return (IntObjectMap<T>) EMPTY_INT_OBJECT_MAP;
    }
+
+   public static <T> IntObjectMap<T> unmodifiableMap( final IntObjectMap<T> map ) {
+      if( map instanceof UnmodifiableIntObjectMap ) {
+         return map;
+      }
+      return new UnmodifiableIntObjectMap<T>( map );
+   }
    
+   public static IntIntMap unmodifiableMap( final IntIntMap map ) {
+      if( map instanceof UnmodifiableIntIntMap ) {
+         return map;
+      }
+      return new UnmodifiableIntIntMap( map );
+   }
    
+   public static IntLongMap unmodifiableMap( final IntLongMap map ) {
+      if( map instanceof UnmodifiableIntLongMap ) {
+         return map;
+      }
+      return new UnmodifiableIntLongMap( map );
+   }
    
+   public static IntDoubleMap unmodifiableMap( final IntDoubleMap map ) {
+      if( map instanceof UnmodifiableIntDoubleMap ) {
+         return map;
+      }
+      return new UnmodifiableIntDoubleMap( map );
+   }
+
+   public static IntSet unmodifiableIntSet( final IntSet c ) {
+      if( c instanceof UnmodifiableIntSet ) {
+         return c;
+      }
+      return new UnmodifiableIntSet( c );
+   }
+   
+   public static IntList unmodifiableIntList( final IntList c ) {
+      if( c instanceof UnmodifiableIntList ) {
+         return c;
+      }
+      return new UnmodifiableIntList( c );
+   }
+   
+   public static IntCollection unmodifiableIntCollection( final IntCollection c ) {
+      if( c instanceof UnmodifiableIntCollection ) {
+         return c;
+      }
+      return new UnmodifiableIntCollection( c );
+   }
+
    ////////////////////////////////////////////////////////////////
-   
-   
+
    private static IntIterator emptyIntIterator = new IntIterator() {
 
       @Override
@@ -90,11 +138,11 @@ public class FastCollections {
       public void remove() {
          throw new NoSuchElementException();
       }
-      
+
    };
-   
+
    private static IntCollection EMPTY_INT_COLLECTION = new EmptyIntCollection();
-   
+
    private static class EmptyIntCollection implements IntCollection {
 
       @Override
@@ -150,11 +198,11 @@ public class FastCollections {
       public Iterator<IntCursor> iterator() {
          return Collections.<IntCursor>emptyIterator();
       }
-      
+
    }
-   
+
    private static IntList EMPTY_INT_LIST = new EmptyIntList();
-   
+
    private static class EmptyIntList extends EmptyIntCollection implements IntList {
 
       @Override
@@ -168,12 +216,12 @@ public class FastCollections {
       }
 
       @Override
-      public int get(int index) {
+      public int get( int index ) {
          throw new NoSuchElementException();
       }
-      
+
       @Override
-      public int set(int index, int value) {
+      public int set( int index, int value ) {
          throw new NoSuchElementException();
       }
 
@@ -194,15 +242,15 @@ public class FastCollections {
       @Override
       public void sort( IntComparator comparator ) {
       }
-      
+
    }
-   
+
    private static IntSet EMPTY_INT_SET = new EmptyIntSet();
-   
+
    private static class EmptyIntSet extends EmptyIntCollection implements IntSet {
-      
+
    }
-   
+
    private static IntIntMap EMPTY_INT_INT_MAP = new IntIntMap() {
 
       @Override
@@ -227,7 +275,7 @@ public class FastCollections {
 
       @Override
       public int get( int key ) {
-         throw new NoSuchElementException("Key "+key+" not found.");
+         throw new NoSuchElementException( "Key " + key + " not found." );
       }
 
       @Override
@@ -248,9 +296,9 @@ public class FastCollections {
       @Override
       public void clear() {
       }
-      
+
    };
-   
+
    private static IntDoubleMap EMPTY_INT_DOUBLE_MAP = new IntDoubleMap() {
 
       @Override
@@ -275,7 +323,7 @@ public class FastCollections {
 
       @Override
       public double get( int key ) {
-         throw new NoSuchElementException("Key "+key+" not found.");
+         throw new NoSuchElementException( "Key " + key + " not found." );
       }
 
       @Override
@@ -296,55 +344,463 @@ public class FastCollections {
       @Override
       public void clear() {
       }
-      
+
    };
-   
+
    @SuppressWarnings( "rawtypes" )
    private static IntObjectMap EMPTY_INT_OBJECT_MAP = new IntObjectMap() {
-      
+
       @Override
       public Iterator<IntObjectEntry> iterator() {
          return Collections.<IntObjectEntry>emptyIterator();
       }
-      
+
       @Override
       public boolean containsKey( int key ) {
          return false;
       }
-      
+
       @Override
       public Object put( int key, Object value ) {
          throw new UnsupportedOperationException();
       }
-      
+
       @Override
       public Object remove( int key ) {
          return null;
       }
-      
+
       @Override
       public Object get( int key ) {
          return null;
       }
-      
+
       @Override
       public int size() {
          return 0;
       }
-      
+
       @Override
       public boolean isEmpty() {
          return true;
       }
-      
+
       @Override
       public IntIterator keyIterator() {
          return emptyIntIterator;
       }
-      
+
       @Override
       public void clear() {
       }
-      
+
    };
+   
+   private static class UnmodifiableIntIntMap implements IntIntMap {
+      
+      private  final IntIntMap _map;
+      
+      UnmodifiableIntIntMap( final IntIntMap map ) {
+         _map = map;
+      }
+
+      @Override
+      public Iterator<IntIntEntry> iterator() {
+         return new ReadOnlyIterator<>( _map.iterator() );
+      }
+
+      @Override
+      public boolean containsKey( int key ) {
+         return _map.containsKey( key );
+      }
+
+      @Override
+      public boolean put( int key, int value ) {
+         throw new UnsupportedOperationException();
+      }
+
+      @Override
+      public boolean remove( int key ) {
+         throw new UnsupportedOperationException();
+      }
+
+      @Override
+      public int get( int key ) {
+         return _map.get( key );
+      }
+
+      @Override
+      public int size() {
+         return _map.size();
+      }
+
+      @Override
+      public boolean isEmpty() {
+         return _map.isEmpty();
+      }
+
+      @Override
+      public IntIterator keyIterator() {
+         return new ReadOnlyIntIterator( _map.keyIterator() );
+      }
+
+      @Override
+      public void clear() {
+         throw new UnsupportedOperationException();
+      }
+      
+   }
+   
+   private static class UnmodifiableIntLongMap implements IntLongMap {
+      
+      private  final IntLongMap _map;
+      
+      UnmodifiableIntLongMap( final IntLongMap map ) {
+         _map = map;
+      }
+
+      @Override
+      public Iterator<IntLongEntry> iterator() {
+         return new ReadOnlyIterator<>( _map.iterator() );
+      }
+
+      @Override
+      public boolean containsKey( int key ) {
+         return _map.containsKey( key );
+      }
+
+      @Override
+      public boolean put( int key, long value ) {
+         throw new UnsupportedOperationException();
+      }
+
+      @Override
+      public boolean remove( int key ) {
+         throw new UnsupportedOperationException();
+      }
+
+      @Override
+      public long get( int key ) {
+         return _map.get( key );
+      }
+
+      @Override
+      public int size() {
+         return _map.size();
+      }
+
+      @Override
+      public boolean isEmpty() {
+         return _map.isEmpty();
+      }
+
+      @Override
+      public IntIterator keyIterator() {
+         return new ReadOnlyIntIterator( _map.keyIterator() );
+      }
+
+      @Override
+      public void clear() {
+         throw new UnsupportedOperationException();
+      }
+      
+   }
+   
+private static class UnmodifiableIntDoubleMap implements IntDoubleMap {
+      
+      private  final IntDoubleMap _map;
+      
+      UnmodifiableIntDoubleMap( final IntDoubleMap map ) {
+         _map = map;
+      }
+
+      @Override
+      public Iterator<IntDoubleEntry> iterator() {
+         return new ReadOnlyIterator<>( _map.iterator() );
+      }
+
+      @Override
+      public boolean containsKey( int key ) {
+         return _map.containsKey( key );
+      }
+
+      @Override
+      public boolean put( int key, double value ) {
+         throw new UnsupportedOperationException();
+      }
+
+      @Override
+      public boolean remove( int key ) {
+         throw new UnsupportedOperationException();
+      }
+
+      @Override
+      public double get( int key ) {
+         return _map.get( key );
+      }
+
+      @Override
+      public int size() {
+         return _map.size();
+      }
+
+      @Override
+      public boolean isEmpty() {
+         return _map.isEmpty();
+      }
+
+      @Override
+      public IntIterator keyIterator() {
+         return new ReadOnlyIntIterator( _map.keyIterator() );
+      }
+
+      @Override
+      public void clear() {
+         throw new UnsupportedOperationException();
+      }
+      
+   }
+
+   private static class UnmodifiableIntObjectMap<T> implements IntObjectMap<T> {
+
+      private final IntObjectMap<T> _map;
+
+      private UnmodifiableIntObjectMap( IntObjectMap<T> map ) {
+         _map = map;
+      }
+
+      @Override
+      public Iterator<IntObjectEntry<T>> iterator() {
+         return new ReadOnlyIterator<>( _map.iterator() );
+      }
+
+      @Override
+      public boolean containsKey( int key ) {
+         return _map.containsKey( key );
+      }
+
+      @Override
+      public T put( int key, T value ) {
+         throw new UnsupportedOperationException();
+      }
+
+      @Override
+      public T remove( int key ) {
+         throw new UnsupportedOperationException();
+      }
+
+      @Override
+      public T get( int key ) {
+         return _map.get( key );
+      }
+
+      @Override
+      public int size() {
+         return _map.size();
+      }
+
+      @Override
+      public boolean isEmpty() {
+         return _map.isEmpty();
+      }
+
+      @Override
+      public IntIterator keyIterator() {
+         return new ReadOnlyIntIterator( _map.keyIterator() );
+      }
+
+      @Override
+      public void clear() {
+         throw new UnsupportedOperationException();
+      }
+
+      @Override
+      public int hashCode() {
+         return _map.hashCode();
+      }
+
+      @Override
+      public boolean equals( Object obj ) {
+         return this == obj || _map.equals( obj );
+      }
+
+   }
+
+   private static class UnmodifiableIntSet extends UnmodifiableIntCollection implements IntSet {
+      
+      UnmodifiableIntSet( IntSet c ) {
+         super( c );
+      }
+      
+   }
+   
+   private static class UnmodifiableIntList extends UnmodifiableIntCollection implements IntList {
+      
+      private final IntList _c;
+      
+      UnmodifiableIntList( IntList c ) {
+         super( c );
+         _c = c;
+      }
+
+      @Override
+      public int getFirst() {
+         return _c.getFirst();
+      }
+
+      @Override
+      public int getLast() {
+         return _c.getLast();
+      }
+
+      @Override
+      public int get( int index ) {
+         return _c.get( index );
+      }
+
+      @Override
+      public int set( int index, int value ) {
+         throw new UnsupportedOperationException();
+      }
+
+      @Override
+      public void removeFirst() {
+         throw new UnsupportedOperationException();
+      }
+
+      @Override
+      public void removeLast() {
+         throw new UnsupportedOperationException();
+      }
+
+      @Override
+      public void sort() {
+         throw new UnsupportedOperationException();
+      }
+
+      @Override
+      public void sort( IntComparator comparator ) {
+         throw new UnsupportedOperationException();
+      }
+      
+   }
+   
+   private static class UnmodifiableIntCollection implements IntCollection {
+
+      private final IntCollection _c;
+
+      UnmodifiableIntCollection( IntCollection c ) {
+         _c = c;
+      }
+
+      @Override
+      public Iterator<IntCursor> iterator() {
+         return new ReadOnlyIterator<>( _c.iterator() );
+      }
+
+      @Override
+      public boolean add( int value ) {
+         throw new UnsupportedOperationException();
+      }
+
+      @Override
+      public boolean addAll( IntCollection elements ) {
+         throw new UnsupportedOperationException();
+      }
+
+      @Override
+      public boolean addAll( int... elements ) {
+         throw new UnsupportedOperationException();
+      }
+
+      @Override
+      public boolean remove( int value ) {
+         throw new UnsupportedOperationException();
+      }
+
+      @Override
+      public int size() {
+         return _c.size();
+      }
+
+      @Override
+      public boolean isEmpty() {
+         return _c.isEmpty();
+      }
+
+      @Override
+      public boolean contains( int value ) {
+         return _c.contains( value );
+      }
+
+      @Override
+      public boolean containsAll( IntCollection c ) {
+         return _c.containsAll( c );
+      }
+
+      @Override
+      public IntIterator intIterator() {
+         return new ReadOnlyIntIterator( _c.intIterator() );
+      }
+
+      @Override
+      public void clear() {
+         throw new UnsupportedOperationException();
+      }
+
+   }
+
+   private static class ReadOnlyIntIterator implements IntIterator {
+
+      private final IntIterator _itr;
+
+      ReadOnlyIntIterator( final IntIterator itr ) {
+         _itr = itr;
+      }
+
+      @Override
+      public int nextInt() {
+         return _itr.nextInt();
+      }
+
+      @Override
+      public boolean hasNext() {
+         return _itr.hasNext();
+      }
+
+      @Override
+      public void remove() {
+         throw new UnsupportedOperationException();
+      }
+
+   }
+
+   private static class ReadOnlyIterator<T> implements Iterator<T> {
+
+      private final Iterator<T> _itr;
+
+      ReadOnlyIterator( final Iterator<T> itr ) {
+         _itr = itr;
+      }
+
+      @Override
+      public T next() {
+         return _itr.next();
+      }
+
+      @Override
+      public boolean hasNext() {
+         return _itr.hasNext();
+      }
+
+      @Override
+      public void remove() {
+         throw new UnsupportedOperationException();
+      }
+
+      public void forEachRemaining( Consumer<? super T> action ) {
+         _itr.forEachRemaining( action );
+      }
+
+   }
 }
--- test/dev/costin/fastcollections/tools/FastCollectionsTest.java
+++ test/dev/costin/fastcollections/tools/FastCollectionsTest.java
@@ -1,13 +1,33 @@
 package dev.costin.fastcollections.tools;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.math.BigDecimal;
+import java.util.Iterator;
 
 import org.junit.Test;
 
+import dev.costin.fastcollections.IntCursor;
+import dev.costin.fastcollections.IntIterator;
+import dev.costin.fastcollections.lists.IntList;
+import dev.costin.fastcollections.lists.impl.IntArrayList;
 import dev.costin.fastcollections.maps.IntDoubleMap;
+import dev.costin.fastcollections.maps.IntDoubleMap.IntDoubleEntry;
 import dev.costin.fastcollections.maps.IntIntMap;
+import dev.costin.fastcollections.maps.IntIntMap.IntIntEntry;
+import dev.costin.fastcollections.maps.IntLongMap;
+import dev.costin.fastcollections.maps.IntLongMap.IntLongEntry;
 import dev.costin.fastcollections.maps.IntObjectMap;
+import dev.costin.fastcollections.maps.IntObjectMap.IntObjectEntry;
+import dev.costin.fastcollections.maps.impl.IntDoubleGrowingMap;
+import dev.costin.fastcollections.maps.impl.IntIntGrowingMap;
+import dev.costin.fastcollections.maps.impl.IntLongGrowingMap;
+import dev.costin.fastcollections.maps.impl.IntObjectGrowingMap;
 import dev.costin.fastcollections.sets.IntSet;
+import dev.costin.fastcollections.sets.impl.IntGrowingSet;
 
 public class FastCollectionsTest {
 
@@ -101,6 +121,372 @@ public class FastCollectionsTest {
          fail( "Missing UnsupportedOperationException on add( int )" );
       }
       
+   }
+   
+   @Test
+   public void testUnmodifiableIntObjectMap() {
+      IntObjectMap<BigDecimal> m = new IntObjectGrowingMap<>();
+      
+      m.put( 1, BigDecimal.valueOf( 1 ) );
+      
+      IntObjectMap<BigDecimal> u = FastCollections.unmodifiableMap( m );
+      
+      assertEquals( BigDecimal.ONE, u.get( 1 ) );
+      
+      m.put( 2, BigDecimal.valueOf( 2 ) );
+      
+      assertEquals( BigDecimal.valueOf( 2 ), u.get( 2 ) );
+      
+      try {
+         u.remove( 1 );
+         fail();
+      }
+      catch( UnsupportedOperationException e ) {
+         
+      }
+      
+      try {
+         u.put( 3, BigDecimal.valueOf( 3 ) );
+         fail();
+      }
+      catch( UnsupportedOperationException e ) {
+         
+      }
+      
+      try {
+         u.clear();
+         fail();
+      }
+      catch( UnsupportedOperationException e ) {
+         
+      }
+      
+      IntIterator itr1 = u.keyIterator();
+      itr1.nextInt();
+      
+      try {
+         itr1.remove();
+         fail();
+      }
+      catch( UnsupportedOperationException e ) {
+         
+      }
+      
+      Iterator<IntObjectEntry<BigDecimal>> itr2 = u.iterator();
+      itr2.next();
+      
+      try {
+         itr2.remove();
+         fail();
+      }
+      catch( UnsupportedOperationException e ) {
+         
+      }
+   }
+   
+   @Test
+   public void testUnmodifiableIntIntMap() {
+      IntIntMap m = new IntIntGrowingMap();
+      
+      m.put( 1, 1  );
+      
+      IntIntMap u = FastCollections.unmodifiableMap( m );
+      
+      assertEquals( 1, u.get( 1 ) );
+      
+      m.put( 2, 2 );
+      
+      assertEquals( 2, u.get( 2 ) );
+      
+      try {
+         u.remove( 1 );
+         fail();
+      }
+      catch( UnsupportedOperationException e ) {
+         
+      }
+      
+      try {
+         u.put( 3, 3 );
+         fail();
+      }
+      catch( UnsupportedOperationException e ) {
+         
+      }
+      
+      try {
+         u.clear();
+         fail();
+      }
+      catch( UnsupportedOperationException e ) {
+         
+      }
+      
+      IntIterator itr1 = u.keyIterator();
+      itr1.nextInt();
+      
+      try {
+         itr1.remove();
+         fail();
+      }
+      catch( UnsupportedOperationException e ) {
+         
+      }
+      
+      Iterator<IntIntEntry> itr2 = u.iterator();
+      itr2.next();
+      
+      try {
+         itr2.remove();
+         fail();
+      }
+      catch( UnsupportedOperationException e ) {
+         
+      }
+   }
+   
+   @Test
+   public void testUnmodifiableIntLongMap() {
+      IntLongMap m = new IntLongGrowingMap();
+      
+      m.put( 1, 1  );
+      
+      IntLongMap u = FastCollections.unmodifiableMap( m );
+      
+      assertEquals( 1, u.get( 1 ) );
+      
+      m.put( 2, 2 );
+      
+      assertEquals( 2, u.get( 2 ) );
+      
+      try {
+         u.remove( 1 );
+         fail();
+      }
+      catch( UnsupportedOperationException e ) {
+         
+      }
+      
+      try {
+         u.put( 3, 3 );
+         fail();
+      }
+      catch( UnsupportedOperationException e ) {
+         
+      }
+      
+      try {
+         u.clear();
+         fail();
+      }
+      catch( UnsupportedOperationException e ) {
+         
+      }
+      
+      IntIterator itr1 = u.keyIterator();
+      itr1.nextInt();
+      
+      try {
+         itr1.remove();
+         fail();
+      }
+      catch( UnsupportedOperationException e ) {
+         
+      }
+      
+      Iterator<IntLongEntry> itr2 = u.iterator();
+      itr2.next();
+      
+      try {
+         itr2.remove();
+         fail();
+      }
+      catch( UnsupportedOperationException e ) {
+         
+      }
+   }
+   
+   @Test
+   public void testUnmodifiableIntDoubleMap() {
+      IntDoubleMap m = new IntDoubleGrowingMap();
+      
+      m.put( 1, 1  );
+      
+      IntDoubleMap u = FastCollections.unmodifiableMap( m );
+      
+      assertEquals( Double.valueOf( 1 ), Double.valueOf( u.get( 1 ) ) );
+      
+      m.put( 2, 2 );
+      
+      assertEquals( Double.valueOf( 2 ), Double.valueOf( u.get( 2 ) ) );
+      
+      try {
+         u.remove( 1 );
+         fail();
+      }
+      catch( UnsupportedOperationException e ) {
+         
+      }
+      
+      try {
+         u.put( 3, 3 );
+         fail();
+      }
+      catch( UnsupportedOperationException e ) {
+         
+      }
+      
+      try {
+         u.clear();
+         fail();
+      }
+      catch( UnsupportedOperationException e ) {
+         
+      }
+      
+      IntIterator itr1 = u.keyIterator();
+      itr1.nextInt();
+      
+      try {
+         itr1.remove();
+         fail();
+      }
+      catch( UnsupportedOperationException e ) {
+         
+      }
+      
+      Iterator<IntDoubleEntry> itr2 = u.iterator();
+      itr2.next();
+      
+      try {
+         itr2.remove();
+         fail();
+      }
+      catch( UnsupportedOperationException e ) {
+         
+      }
+   }
+   
+   @Test
+   public void testUnmodifiableIntList() {
+      IntList m = new IntArrayList();
+      
+      m.add( 1 );
+      
+      IntList u = FastCollections.unmodifiableIntList( m );
+      
+      assertEquals( 1, u.get( 0 ) );
+      
+      m.add( 2 );
+      
+      assertEquals( 2, u.get( 1 ) );
+      
+      try {
+         u.remove( 1 );
+         fail();
+      }
+      catch( UnsupportedOperationException e ) {
+         
+      }
+      
+      try {
+         u.add( 3 );
+         fail();
+      }
+      catch( UnsupportedOperationException e ) {
+         
+      }
+      
+      try {
+         u.clear();
+         fail();
+      }
+      catch( UnsupportedOperationException e ) {
+         
+      }
+      
+      IntIterator itr1 = u.intIterator();
+      itr1.nextInt();
+      
+      try {
+         itr1.remove();
+         fail();
+      }
+      catch( UnsupportedOperationException e ) {
+         
+      }
+      
+      Iterator<IntCursor> itr2 = u.iterator();
+      itr2.next();
+      
+      try {
+         itr2.remove();
+         fail();
+      }
+      catch( UnsupportedOperationException e ) {
+         
+      }
+   }
+   
+   @Test
+   public void testUnmodifiableIntSet() {
+      IntSet m = new IntGrowingSet();
+      
+      m.add( 1 );
+      
+      IntSet u = FastCollections.unmodifiableIntSet( m );
+      
+      assertTrue( u.contains( 1 ) );
+      
+      m.add( 2 );
+      
+      assertTrue( u.contains( 2 ) );
+      
+      try {
+         u.remove( 1 );
+         fail();
+      }
+      catch( UnsupportedOperationException e ) {
+         
+      }
+      
+      try {
+         u.add( 3 );
+         fail();
+      }
+      catch( UnsupportedOperationException e ) {
+         
+      }
+      
+      try {
+         u.clear();
+         fail();
+      }
+      catch( UnsupportedOperationException e ) {
+         
+      }
+      
+      IntIterator itr1 = u.intIterator();
+      itr1.nextInt();
+      
+      try {
+         itr1.remove();
+         fail();
+      }
+      catch( UnsupportedOperationException e ) {
+         
+      }
+      
+      Iterator<IntCursor> itr2 = u.iterator();
+      itr2.next();
+      
+      try {
+         itr2.remove();
+         fail();
+      }
+      catch( UnsupportedOperationException e ) {
+         
+      }
    }
    
 }
