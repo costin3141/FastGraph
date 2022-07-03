40c7bd8b8aee728b6c6f7e13cc253c56eb1973c3
Stefan C. Ionescu <Stefan.Ionescu@dematic.com>
2022-07-03 17:55:55 +0200

    Fill for long arrays.

--- src/dev/costin/fastcollections/tools/CollectionUtils.java
+++ src/dev/costin/fastcollections/tools/CollectionUtils.java
@@ -26,4 +26,59 @@ public class CollectionUtils {
                   (delta < i) ? delta : i );
       }
    }
+   
+   /**
+    * A fast fill for long arrays.
+    * 
+    * @param array
+    *           The array to be filled.
+    * @param start
+    *           The index at which to start filling.
+    * @param end
+    *           The index at which to stop filling. {@code array[end]} will
+    *           not be changed.
+    * @param val
+    *           The value to set.
+    */
+   public static void fill( final long[] array, final int start, final int end, long val ) {
+      final int len = end - start;
+      if( len > 0 ) {
+         array[start] = val;
+      }
+      for( int i = 1; i < len; i += i ) {
+         final int delta = (len - i);
+         System.arraycopy( array, start, array, start + i,
+                  (delta < i) ? delta : i );
+      }
+   }
+   
+   public static int getUpperInt( long l ) {
+      return (int)( l >>> 32);
+   }
+   
+   public static int getLowerInt( long l ) {
+      return (int) l;
+   }
+   
+   public static long combineToLong( int upper, int lower ) {
+      return ((long)upper << 32) | ((long)lower & 0xffffffffl );
+//      long l;
+//      if (lower < 0) {
+//         l = upper + 1;
+//      }
+//      else {
+//         l = upper;
+//      }
+//      l <<= 32;
+//      l += lower;
+//      return l;
+   }
+   
+   public static long setLowerInt( long l, int lower ) {
+      return (l & 0xffffffff00000000l) | ((long)lower & 0xffffffffl );
+   }
+   
+   public static long setUpperInt( long l, int upper ) {
+      return ((long)l & 0xffffffffl ) | ((long)upper << 32);
+   }
 }
