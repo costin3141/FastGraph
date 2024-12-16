a73476f5ff4ba08874e7ba18ea38ba932b3f420c
Costin <Costin@BUUMBUUM>
2024-12-16 11:37:21 +0100

    Fix and improved sorting.

--- src/dev/costin/fastcollections/tools/algorithms/Sort.java
+++ src/dev/costin/fastcollections/tools/algorithms/Sort.java
@@ -1,17 +1,29 @@
 package dev.costin.fastcollections.tools.algorithms;
 
 import dev.costin.fastcollections.IntComparator;
+import dev.costin.fastcollections.lists.IntList;
 
 public class Sort {
 
    private Sort() {
    }
 
-   public static void sort( final int list[], final int from, final int length, final IntComparator cmp ) {
-//      mergeSort( list, from, , cmp );
-      timSort( list, from, from + length - 1, cmp );
+   public static void sort( final int list[], final int from, final int to, final IntComparator cmp ) {
+      timSort( list, from, to - 1, cmp );
    }
 
+   public static boolean isSorted( IntList l, IntComparator cmp ) {
+      for( int i = 0; i < l.size() - 1; i++ ) {
+         if( cmp.compare( l.get( i ), l.get( i + 1 ) ) > 0 ) {
+            return false;
+         }
+      }
+      
+      return true;
+   }
+   
+   
+///////////////////////////////////////////////////////////////
    static int MIN_MERGE = 32;
 
    private static int minRunLength( int n ) {
@@ -26,12 +38,12 @@ public class Sort {
       return n + r;
    }
 
-   public static void insertionSort( final int[] list, final int left, final int right, final IntComparator cmp ) {
-      for( int i = left + 1; i <= right; i++ ) {
+   private static void insertionSort( final int[] list, final int from, final int to, final IntComparator cmp ) {
+      for( int i = from + 1; i <= to; i++ ) {
          int temp = list[i];
          int j = i - 1;
 
-         while( j >= left && cmp.compare( list[j], temp ) > 0 ) {
+         while( j >= from && cmp.compare( list[j], temp ) > 0 ) {
             list[j + 1] = list[j];
             j--;
          }
@@ -42,122 +54,63 @@ public class Sort {
    private static void timSort( final int[] list, final int from, final int to, final IntComparator cmp ) {
       int minRun = minRunLength( MIN_MERGE );
 
-      // Sort individual subarrays of size RUN
       for( int i = from; i <= to; i += minRun ) {
          insertionSort( list, i, Math.min( ( i + MIN_MERGE - 1 ), to ), cmp );
       }
 
       final int length = to - from + 1;
+      final int[] buffer = new int[length];
 
-      final int[] L = new int[length - (length >> 2)];
-      final int[] R = new int[length >> 1];
+      for( int size = minRun; size < length; size = ( size << 1 ) ) {
 
-      for( int size = minRun; size < length; size = (size << 1) ) {
-         for( int left = from; left <= to; left += (size << 1) ) {
+         for( int left = from; left <= to; left += ( size << 1 ) ) {
 
-            final int mid = left + size - 1;
-            final int right = Math.min( ( left + (size << 1) - 1 ), to );
+            int mid = left + size - 1;
+            int right = Math.min( ( left + ( size << 1 ) - 1 ), to );
 
             if( mid < right ) {
-               merge( list, left, mid, right, cmp, L, R );
+               merge( list, left, mid, right, cmp, buffer );
             }
          }
       }
    }
 
-   //////////// merge sort
+   private static void merge( int list[], int l, int m, int r, final IntComparator cmp, final int buffer[] ) {
+      final int n1 = m - l + 1;
+      final int n2 = r - m;
+      final int endR = n1 + n2;
 
-//   private static void mergeSort( int list[], int l, int r, final IntComparator cmp ) {
-//      if( l < r ) {
-//         int m = l + ( r - l ) / 2;
-//         mergeSort( list, l, m, cmp );
-//         mergeSort( list, m + 1, r, cmp );
-//         merge( list, l, m, r, cmp );
-//      }
-//   }
-
-   private static void merge( final int list[], final int l, final int m, final int r, final IntComparator cmp,
-         final int[] L, final int[] R ) {
-      final int leftLength = m - l + 1;
-      final int rightLength = r - m;
-
-      for( int i = 0; i < leftLength; i++ ) {
-         L[i] = list[l + i];
+      for( int i = 0; i < n1; i++ ) {
+         buffer[i] = list[l + i];
       }
 
-      for( int i = 0; i < rightLength; i++ ) {
-         R[i] = list[m + 1 + i];
+      for( int i = n1; i < endR; i++ ) {
+         buffer[i] = list[m + 1 - n1 + i];
       }
 
-      int i = 0, j = 0, k = l;
+      int i = 0, j = n1, k = l;
 
-      while( i < leftLength && j < rightLength ) {
-         if( cmp.compare( L[i], R[j] ) <= 0 ) {
-            list[k++] = L[i++];
+      while( i < n1 && j < endR ) {
+         final int lValue = buffer[i];
+         final int rValue = buffer[j];
+
+         if( cmp.compare( lValue, rValue ) <= 0 ) {
+            list[k++] = lValue;
+            ++i;
          }
          else {
-            list[k++] = R[j++];
+            list[k++] = rValue;
+            ++j;
          }
       }
 
-      while( i < leftLength ) {
-         list[k++] = L[i++];
+      while( i < n1 ) {
+         list[k++] = buffer[i++];
       }
 
-      while( j < rightLength ) {
-         list[k++] = R[j++];
+      while( j < endR ) {
+         list[k++] = buffer[j++];
       }
    }
 
-//   public static void main( String[] args ) {
-//      final IntComparator cmp = new IntComparator() {
-//         
-//         @Override
-//         public int compare( int i1, int i2 ) {
-//            return Integer.compare( i2, i1 );
-//         }
-//      };
-//      
-//      Random rnd = new Random( new Date().getTime() );
-//      final int n = 100000;
-//      final int r = 1000;
-//      
-//      IntArrayList l = new IntArrayList(n);
-//      for( int i=0; i<n; i++ ) {
-//         l.add( i );
-//      }
-//      
-//      FastCollections.shuffle( l, rnd );
-//      
-//      long t = System.currentTimeMillis();
-//      for( int i=0; i<r; i++ ) {
-////         System.out.println( i );
-//         FastCollections.shuffle( l, rnd );
-//         sort( l._list, 0, n-1, cmp );
-////         mergeSort( l._list, 0, n-1, cmp );
-//      }
-//      System.out.println( System.currentTimeMillis() - t );
-//      
-//      t = System.currentTimeMillis();
-//      for( int i=0; i<r; i++ ) {
-////         System.out.println( i );
-//         FastCollections.shuffle( l, rnd );
-//         sort( l._list, 0, n-1, cmp );
-////         mergeSort( l._list, 0, n-1, cmp );
-//      }
-//      System.out.println( System.currentTimeMillis() - t );
-//      
-//      t = System.currentTimeMillis();
-//      for( int i=0; i<r; i++ ) {
-//         IntArrayList l2 = new IntArrayList( rnd.nextInt(n) + 1 );
-//         for( int j=0; j<n; j++ ) {
-//            l2.add( i );
-//         }
-////         System.out.println( i );
-//         FastCollections.shuffle( l2, rnd );
-//         sort( l2._list, 0, l2.size() - 1, cmp );
-////         mergeSort( l._list, 0, n-1, cmp );
-//      }
-//      System.out.println( System.currentTimeMillis() - t );
-//   }
 }
